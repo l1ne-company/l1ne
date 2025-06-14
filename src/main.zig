42d9644825b8b7bae5b6dfc39f96a7f5ac099462
c0utin <rafaelcouto111@gmail.com>
2025-06-13 23:54:37 -0300

    [global] love4game

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,3 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
-
 pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
